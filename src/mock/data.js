f36569c40b99eee4b887b1371503adb9d9c4ee77
jeremydmarx <jeremydmarx@gmail.com>
2021-08-13 02:35:20 -0500

    cleaned up jammming appearance

--- src/mock/data.js
+++ src/mock/data.js
@@ -35,11 +35,11 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'jammingImg.png',
+    img: 'jammmingImg.jpg',
     title: 'Jammming',
     info: 'Capstone Project for React Unit of Codecademy Web Development Career Path Certificate.  Design boiler plate provided.',
     info2: 'I refactored and set up The Context API for Global State and added the logo at the top, clearing the search bar. Deployed using Surge.sh',
-    url: 'http://jamming-codecademy.surge.sh/',
+    url: 'https://spotify-playlist-make-cc.herokuapp.com/',
     repo: 'https://github.com/jeremydmarx813/jamming-codecademy-project', 
   }
   // ,
